73588e553ddd048fc0fea81de24c1ac3e52ee65b
Xue Qian <xueqian0321@126.com>
2014-11-14 11:17:56 +0800

    modify show category at goodList page

--- src/main/webapp/client/app/scripts/controllers/itemList.js
+++ src/main/webapp/client/app/scripts/controllers/itemList.js
@@ -1,28 +1,37 @@
 'use strict';
 
 angular.module('letusgoApp')
-  .controller('itemListCtrl', function ($scope, CartService, ItemService) {
+    .controller('itemListCtrl', function ($scope, CartService, ItemService,CategoryService) {
 
-    function EventName() {
-      this.PARENT_ITEMLIST_ACTIVE = 'parent_itemListActive';
-      this.PARENT_TOTAL_COUNT = 'parent_totalCount';
-    }
+        function EventName() {
+            this.PARENT_ITEMLIST_ACTIVE = 'parent_itemListActive';
+            this.PARENT_TOTAL_COUNT = 'parent_totalCount';
+        }
 
-    $scope.$emit(new EventName().PARENT_ITEMLIST_ACTIVE);
+        $scope.$emit(new EventName().PARENT_ITEMLIST_ACTIVE);
 
-    ItemService.getGoodsItems(function (data) {
-      $scope.itemList = data;
-    });
+        ItemService.getGoodsItems(function (data) {
 
-    $scope.$emit(new EventName().PARENT_TOTAL_COUNT);
+            _(data).forEach(function (item) {
 
-    $scope.addToCart = function (item) {
+                CategoryService.getCategory(item.categoryId, function (data) {
+                    item.category = data.name;
+                });
 
-      CartService.addToCart(item, function () {
+            });
+
+            $scope.items = data;
+        });
+
         $scope.$emit(new EventName().PARENT_TOTAL_COUNT);
-      });
-    };
 
-  });
+        $scope.addToCart = function (item) {
+
+            CartService.addToCart(item, function () {
+                $scope.$emit(new EventName().PARENT_TOTAL_COUNT);
+            });
+        };
+
+    });
 
 
