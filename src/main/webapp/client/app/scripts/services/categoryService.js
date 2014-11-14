f968fb6be445b7b54799cb8bcad55046be4a2fb2
Xue Qian <xueqian0321@126.com>
2014-11-14 14:16:02 +0800

    modify page addCategory method post

--- src/main/webapp/client/app/scripts/services/categoryService.js
+++ src/main/webapp/client/app/scripts/services/categoryService.js
@@ -1,69 +1,58 @@
 'use strict';
 
 angular.module('letusgoApp')
-  .service('CategoryService', function ($http) {
+    .service('CategoryService', function ($http) {
 
-    this.getCategories = function (callback) {
-      $http.get('http://localhost:8080/api/categories').
-        success(function (data) {
-          callback(data);
-        });
-    };
+        this.getCategories = function (callback) {
+            $http.get('http://localhost:8080/api/categories').
+                success(function (data) {
+                    callback(data);
+                });
+        };
 
-    this.addCategory = function (category, callback) {
+        this.addCategory = function (category, callback) {
 
-      this.getCategories(function (data) {
+            this.getCategories(function (data) {
 
-        var categoryList = data;
+                if (!hasExistCategory(category, data)) {
 
-        if (!hasExistCategory(category,categoryList)) {
+                    $http.post('http://localhost:8080/api/categories', {id: null, name: category.name})
+                        .success(function (data) {
+                            callback(data);
+                        });
+                }
 
-          setExistCatogory(category,categoryList);
+            });
+        };
+
+        this.deleteCategory = function (id) {
+            $http.delete('http://localhost:8080/api/categories/' + id);
+        };
+
+        this.getCategory = function (id, callback) {
+
+            $http.get('http://localhost:8080/api/categories/' + id)
+                .success(function (data) {
+                    callback(data);
+                });
+        };
+
+        this.modifyCategory = function (id, category, callback) {
+
+            $http.put('http://localhost:8080/api/categories/' + id, {category: category})
+                .success(function (data) {
+                    callback(data);
+                });
+        };
+
+        function hasExistCategory(category, categoryList) {
+
+            return _.any(categoryList, function (aCategory) {
+                return category.name === aCategory.name;
+            });
         }
 
-        $http.post('http://localhost:8080/api/categories', {categoryList: categoryList})
-          .success(function (data) {
-            callback(data);
-          });
-
-      });
-    };
-
-    this.deleteCategory = function (id) {
-      $http.delete('http://localhost:8080/api/categories/' + id);
-    };
-
-    this.getCategory = function (id, callback) {
-
-      $http.get('http://localhost:8080/api/categories/' + id)
-        .success(function (data) {
-          callback(data);
-        });
-    };
-
-    this.modifyCategory = function (id, category, callback) {
-
-      $http.put('http://localhost:8080/api/categories/' + id, {category: category})
-        .success(function (data) {
-          callback(data);
-        });
-    };
-
-    function hasExistCategory(category,categoryList){
-
-      return _.any(categoryList, function (aCategory) {
-        return category.name === aCategory.name;
-      });
-    }
-
-    function setExistCatogory(category,categoryList){
-      var id = _.pluck(categoryList, 'id');
-      category.id = _.max(id) + 1;
-
-      categoryList.push(category);
-    }
-
-  });
+    });
 
 
 
